170dd7c17e93939672ddd8336c206f03f24f03d5
Muharrem Süslü <muharremssl.001@gmail.com>
2023-03-23 00:55:59 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 #include "stm32l4xx.h"
 #include "Delay_ms.h"
 
-volatile uint16_t v_lvl[2] = {0,0};
+volatile uint16_t v_lvl[2];
 volatile uint16_t adc_val[2];
 volatile int count = 0;
 
 void ADC_Init(void);
-void ADC_Enable (void);
+void ADC_enable (void);
 void ADC_Start(void);
 void DMA_Init (void);
 void  DMA_Config (volatile uint32_t srcAdd, volatile uint32_t destAdd, volatile uint8_t size);
@@ -19,17 +19,21 @@ int main (void)
   GPIOB->MODER |= (1<<26);
   TIM2Config();
   ADC_Init();
-  ADC_Enable();
+  ADC_enable();
   DMA_Init();
 
-  DMA_Config((volatile uint32_t) &ADC1->DR, (volatile uint32_t) adc_val, 2);
+  //DMA_Config((volatile uint32_t) &ADC1->DR, (volatile uint32_t) adc_val, 2);
 
-  ADC_Start();
+
 
   while (1)
   {
+     ADC_Start();
+     DMA_Config((volatile uint32_t) &ADC1->DR, (volatile uint32_t) adc_val, 2);
+
      v_lvl[1] = (adc_val[1]*2/4095)*3.3;
      v_lvl[2] = (adc_val[2]*2/4095)*3.3;
+
 
   }
 return 0;
@@ -58,17 +62,17 @@ void ADC_Init (void)
   ADC1->CFGR &= ~(3<<3); // set resolution to 12-bit
   ADC1->CFGR |= (1<<13); // continuous conversion mode
   ADC1->CFGR &= ~(1<<5); // right Alignment
-  ADC1->SMPR1 &= ~(7<<0) & ~(7<<3); // reset Sampling rate for channel 0 & 1
-  ADC1->SMPR1 |= (7<<0) | (7<<3); // 640.5 Sampling rate for channel 0 & 1
+  ADC1->SMPR1 &= ~(7<<3) & ~(7<<12); // reset Sampling rate for P 0 & 3
+  ADC1->SMPR1 |= (7<<3) | (7<<12);  //  640.5 Sampling rate for P 0 & 3
   ADC1->SQR1 &= ~(0xf);
   ADC1->SQR1 |= (1<<0); // for 2 channel
 
   GPIOC->MODER &= ~(0xffffffff); // reset the whole register
-  GPIOC->MODER |= (3<<0) | (3<<2); // set the analog mode to pin PC0 and PC1
+  GPIOC->MODER |= (3<<0) | (3<<6); // set the analog mode to pin PC0 and PC3
 
   ADC1->CFGR |= (1<<0); // Enable DMA
   ADC1->SQR1 &= ~(0xf<<6) & ~(0xf<<12);
-  ADC1->SQR1 |= (1<<6) | (1<<12);
+  ADC1->SQR1 |= (1<<6) | (4<<12);
 }
 
 void DMA_Init (void)
@@ -87,12 +91,11 @@ void  DMA_Config (volatile uint32_t srcAdd, volatile uint32_t destAdd, volatile 
   DMA1_Channel1->CNDTR = size; // size of the transfer
   DMA1_Channel1->CPAR = srcAdd; // adress of the peripheral
   DMA1_Channel1->CMAR = destAdd; // adress of the source
-
   DMA1_Channel1->CCR |= (1<<0); // enable DMA1
 
 }
 
-void ADC_Enable (void)
+void ADC_enable (void)
 {
 	/************** STEPS TO FOLLOW *****************
 	1. Enable the ADC by setting ADON bit in CR2
