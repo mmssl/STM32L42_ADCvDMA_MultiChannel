6b84de5794a87bad8daca82ca5a3e7107ec9eeff
Muharrem Süslü <muharremssl.001@gmail.com>
2023-03-23 23:04:05 +0300

    Update main.c

--- main.c
+++ main.c
@@ -16,25 +16,19 @@ int main (void)
 {
   SystemInit();
   GPIOB->MODER &= ~(0xffffffff);
-  GPIOB->MODER |= (1<<26);
+  GPIOB->MODER |=  (1<<26);
   TIM2Config();
   ADC_Init();
   ADC_enable();
   DMA_Init();
 
-  //DMA_Config((volatile uint32_t) &ADC1->DR, (volatile uint32_t) adc_val, 2);
-
-
-
   while (1)
   {
      ADC_Start();
-     DMA_Config((volatile uint32_t) &ADC1->DR, (volatile uint32_t) adc_val, 2);
+     DMA_Config(( uint32_t) &ADC1->DR, ( uint32_t) adc_val, 2);
 
      v_lvl[1] = (adc_val[1]*2/4095)*3.3;
      v_lvl[2] = (adc_val[2]*2/4095)*3.3;
-
-
   }
 return 0;
 }
@@ -50,29 +44,29 @@ void ADC_Init (void)
 	6. Set the Regular channel sequence length in ADC_SQR1
 	7. Set the Respective GPIO PINs in the Analog Mode
 	************************************************/
-  RCC->AHB2ENR |= (1<<2) | (1<<13) | (1<<1); // enable GPIOC clock and ADC clock
-  RCC->CCIPR &= ~(3<<28);
-  RCC->CCIPR |= (3<<28);
+  RCC->AHB2ENR |=  (1<<2) | (1<<13) | (1<<1); // enable GPIOC clock and ADC clock
+  RCC->CCIPR   &= ~(3<<28);
+  RCC->CCIPR   |=  (3<<28);
 
   ADC1->CR &= ~(1<<29); // disable the deep-power-down by Setting DEEPWD to 0
-  ADC1->CR |= (1<<0); // disable ADC
-  ADC1->CR |= (1<<28); // enable the voltage regulator
+  ADC1->CR |=  (1<<0); // disable ADC
+  ADC1->CR |=  (1<<28); // enable the voltage regulator
   delay_ms(10);
   //ADC1->CR |= (2<<18); // divided by 4
-  ADC1->CFGR &= ~(3<<3); // set resolution to 12-bit
-  ADC1->CFGR |= (1<<13); // continuous conversion mode
-  ADC1->CFGR &= ~(1<<5); // right Alignment
+  ADC1->CFGR  &= ~(3<<3); // set resolution to 12-bit
+  ADC1->CFGR  |=  (1<<13); // continuous conversion mode
+  ADC1->CFGR  &= ~(1<<5); // right Alignment
   ADC1->SMPR1 &= ~(7<<3) & ~(7<<12); // reset Sampling rate for P 0 & 3
-  ADC1->SMPR1 |= (7<<3) | (7<<12);  //  640.5 Sampling rate for P 0 & 3
-  ADC1->SQR1 &= ~(0xf);
-  ADC1->SQR1 |= (1<<0); // for 2 channel
+  ADC1->SMPR1 |=  (7<<3) | (7<<12);  //  640.5 Sampling rate for P 0 & 3
+  ADC1->SQR1  &= ~(0xf);
+  ADC1->SQR1  |=  (1<<0); // for 2 channel
 
   GPIOC->MODER &= ~(0xffffffff); // reset the whole register
-  GPIOC->MODER |= (3<<0) | (3<<6); // set the analog mode to pin PC0 and PC3
+  GPIOC->MODER |=  (3<<0) | (3<<6); // set the analog mode to pin PC0 and PC3
 
-  ADC1->CFGR |= (1<<0); // Enable DMA
-  ADC1->SQR1 &= ~(0xf<<6) & ~(0xf<<12);
-  ADC1->SQR1 |= (1<<6) | (4<<12);
+  ADC1->CFGR |=  (1<<0); // Enable DMA
+  //ADC1->SQR1 &= ~(0xf<<6) & ~(0xf<<12);
+  ADC1->SQR1 |=  (4<<6) | (1<<12);
 }
 
 void DMA_Init (void)
@@ -80,9 +74,9 @@ void DMA_Init (void)
   RCC->AHB1ENR |= (1<<0); // Enable DMA1 clock
 
   DMA1_Channel1->CCR &= ~(1<<4); // Set the data direction (P-M)
-  DMA1_Channel1->CCR |= (1<<5); // Circullium mode enable
-  DMA1_Channel1->CCR |= (1<<7); // Enable memory increment Mode
-  DMA1_Channel1->CCR |= (1<<8) | (1<<10); // peripheral and memory size setting
+  DMA1_Channel1->CCR |=  (1<<5); // Circullium mode enable
+  DMA1_Channel1->CCR |=  (1<<7); // Enable memory increment Mode
+  DMA1_Channel1->CCR |=  (1<<8) | (1<<10); // peripheral and memory size setting
 }
 
 void  DMA_Config (volatile uint32_t srcAdd, volatile uint32_t destAdd, volatile uint8_t size)
@@ -102,7 +96,7 @@ void ADC_enable (void)
 	2. Wait for ADC to stabilize (approx 10us)
 	************************************************/
   ADC1->ISR |= (1<<0);
-  ADC1->CR |= (1<<0);
+  ADC1->CR  |= (1<<0);
   delay_ms(10);
 }
 
@@ -113,6 +107,6 @@ void ADC_Start(void)
   2. Clear the Status register
 	3. Start the Conversion by Setting the SWSTART bit in CR2
 	*************************************************/
-  //ADC1->ISR |= (1<<0);
+
   ADC1->CR |= (1<<2);
 }
