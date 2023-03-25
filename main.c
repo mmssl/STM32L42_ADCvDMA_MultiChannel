20653cdcc625ccff7630562f7dc28f3cd03a4d41
Muharrem Süslü <muharremssl.001@gmail.com>
2023-03-25 15:08:29 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,15 +2,14 @@
 #include "stm32l4xx.h"
 #include "Delay_ms.h"
 
-volatile uint16_t v_lvl[2];
+volatile float v_lvl[2];
 volatile uint16_t adc_val[2];
-volatile int count = 0;
+
 
 void ADC_Init(void);
 void ADC_enable (void);
-void ADC_Start(void);
 void DMA_Init (void);
-void  DMA_Config (volatile uint32_t srcAdd, volatile uint32_t destAdd, volatile uint8_t size);
+void DMA_Config (volatile uint32_t srcAdd, volatile uint32_t destAdd, volatile uint8_t size);
 
 int main (void)
 {
@@ -24,26 +23,20 @@ int main (void)
 
   while (1)
   {
-     ADC_Start();
+     ADC1->CR |= (1<<2); // start the ADC
      DMA_Config(( uint32_t) &ADC1->DR, ( uint32_t) adc_val, 2);
 
-     v_lvl[1] = (adc_val[1]*2/4095)*3.3;
-     v_lvl[2] = (adc_val[2]*2/4095)*3.3;
+     // Converting ADC values to V levels.
+     v_lvl[0] = (3.3*adc_val[0])/4095;
+     v_lvl[1] = (3.3*adc_val[1])/4095;
+
   }
 return 0;
 }
 
 void ADC_Init (void)
 {
-  /************** STEPS TO FOLLOW *****************
-	1. Enable ADC and GPIO clock
-	2. Set the prescalar in the common control register (ADC_CCR)
-	3. Set the Scan Mode and Resolution in the Control Register 1 (CR1)
-	4. Set the Continuous Conversion, EOC, and Data Alignment in Control Reg 2 (CR2)
-	5. Set the Sampling Time for the channels in ADC_SMPRx
-	6. Set the Regular channel sequence length in ADC_SQR1
-	7. Set the Respective GPIO PINs in the Analog Mode
-	************************************************/
+
   RCC->AHB2ENR |=  (1<<2) | (1<<13) | (1<<1); // enable GPIOC clock and ADC clock
   RCC->CCIPR   &= ~(3<<28);
   RCC->CCIPR   |=  (3<<28);
@@ -66,7 +59,7 @@ void ADC_Init (void)
 
   ADC1->CFGR |=  (1<<0); // Enable DMA
   ADC1->SQR1 &= ~(0xf<<6) & ~(0xf<<12);
-  ADC1->SQR1 |=  (4<<6) | (1<<12);
+  ADC1->SQR1 |=  (1<<6) | (4<<12);
 }
 
 void DMA_Init (void)
@@ -91,22 +84,7 @@ void  DMA_Config (volatile uint32_t srcAdd, volatile uint32_t destAdd, volatile 
 
 void ADC_enable (void)
 {
-	/************** STEPS TO FOLLOW *****************
-	1. Enable the ADC by setting ADON bit in CR2
-	2. Wait for ADC to stabilize (approx 10us)
-	************************************************/
   ADC1->ISR |= (1<<0);
   ADC1->CR  |= (1<<0);
   delay_ms(10);
-}
-
-void ADC_Start(void)
-{
-	/************** STEPS TO FOLLOW *****************
-  1. Set the channel sequence
-  2. Clear the Status register
-	3. Start the Conversion by Setting the SWSTART bit in CR2
-	*************************************************/
-
-  ADC1->CR |= (1<<2);
 }
